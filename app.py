d343c5e15552b4d8907ecc59a669d4c56455f6b2
kimyounggoncom <younggon0925@gmail.com>
2025-01-31 15:11:35 +0900

    최초 커밋 200131 15:10

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def home():   
+    return render_template("index.html")
+
+@app.route('/plus')
+def plus():   
+    return render_template("calculator/plus.html")
+
+
+@app.route('/minus')
+def minus():   
+    return render_template("calculator/minus.html")
+
+@app.route('/multiple')
+def multiple():   
+    return render_template("calculator/multiple.html")
+
+@app.route('/devide')
+def devide():   
+    return render_template("calculator/devide.html")
+
+@app.route('/finance')
+def finance():   
+    return render_template("esg/esg_chatbot/finance.html")
+
+@app.route('/manufacture')
+def manufacture():   
+    return render_template("esg/esg_chatbot/manufacture.html")
+
+@app.route('/retailer')
+def retailer():   
+    return render_template("esg/esg_chatbot/retailer.html")
+
+@app.route('/esg_energy_date')
+def esg_energy_date():   
+    return render_template("esg/esg_platform/esg_energy_date.html")
+
+@app.route('/esg_factory_automation')
+def esg_factory_automation():   
+    return render_template("esg/esg_platform/esg_factory_automation.html")
+
+@app.route('/esg_finance_dashboard')
+def esg_finance_dashboard():   
+    return render_template("esg/esg_platform/esg_finance_dashboard.html")
+
+@app.route('/esg_construction_reports')
+def esg_construction_reports():   
+    return render_template("esg/esg_system/esg_construction_reports.html")
+
+@app.route('/esg_health_fin_qna')
+def esg_health_fin_qna():   
+    return render_template("esg/esg_system/esg_health_fin_qna.html")
+
+@app.route('/retail_finance_automation')
+def retail_finance_automation():   
+    return render_template("esg/esg_system/retail_finance_automation.html")
+
+
+
+
+
+if __name__ == '__main__':  
+   app.run('0.0.0.0',port=5000,debug=True)
---
+++ test.py
@@ -0,0 +1,15 @@
+print("Hello, World!")
+
+a=20
+b=5
+print(str(a) + "+" + str(b) + "=" + str(a + b))
+print(a+b)
+print(f"{a} + {b} = {a + b}")
+
+print(f" {a + b}")         # 20 + 5 = 25
+print(f" {a - b}")         # 20 - 5 = 15
+print(f" {a * b}")         # 20 * 5 = 100
+print(f" {a / b}")       # 20 / 5 = 4.0
+print(f" {a % b}")       # 20 % 5 = 0
+print(f" {a // b}")          # 20 // 5 = 4
+print(f" {a ** b}")    # 20^5 = 3200000
